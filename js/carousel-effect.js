3baa7caa2c7c0a39822594ce6400ac7d60a88a64
jonathanelruso <jonathanmoreraapaza@gmail.com>
2024-12-08 18:10:05 +0000

    final commit

--- js/carousel-effect.js
+++ js/carousel-effect.js
@@ -5,18 +5,18 @@ let currentIndex = 0;
 
 // Función para cambiar la posición del carrusel
 function moveCarousel() {
-    currentIndex = (currentIndex + 1) % TOTAL_ITEMS;
-    let offset = -currentIndex * 100;
-    CAROUSEL_WRAPPER.style.transform = `translateX(${offset}%)`;
-    rotateImages();
+  currentIndex = (currentIndex + 1) % TOTAL_ITEMS;
+  let offset = -currentIndex * 100;
+  CAROUSEL_WRAPPER.style.transform = `translateX(${offset}%)`;
+  rotateImages();
 }
 
 // Función para aplicar la rotación de las imagenes
 function rotateImages() {
-    CAROUSEL_ITEMS.forEach((item, index) => {
-        let rotationDegree = 360 * (index + 1);
-        item.querySelector('img').style.transform = `rotate(${rotationDegree}deg)`;
-    });
+  CAROUSEL_ITEMS.forEach((item, index) => {
+    let rotationDegree = 360 * (index + 1);
+    item.querySelector('img').style.transform = `rotate(${rotationDegree}deg)`;
+  });
 }
 // Configura el intervalo para mover el carrusel cada 2.6 segundos
 setInterval(moveCarousel, 2600);
